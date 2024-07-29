75b0dd5eb43f1e6ce2a7d605c005473de52df6dd
Faraz Ahmed <faraz.ahmed@arkhitech.com>
2024-07-29 17:09:29 +0500

    Tabbing added

--- src/app/letters/index.tsx
+++ src/app/letters/index.tsx
@@ -1,24 +1,42 @@
 "use client";
-import React from 'react';
+import React, { useRef, useState, useEffect, createRef } from 'react';
 
 const Letters = () => {
-  const tabClick = (e: EventTarget) => {
-    // TODO: Handle tabClick;
-  }
+  const numInputs = 5;
+  const [letterRefs, setLetterRefs] = useState<React.RefObject<HTMLInputElement>[]>([]);
+
+  useEffect(() => {
+    setLetterRefs((refs) =>
+      Array(numInputs)
+        .fill(null)
+        .map((_, i) => refs[i] || createRef<HTMLInputElement>())
+    );
+  }, [numInputs]);
+
+  const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+    if (e.key !== 'Shift' && e.key !== 'Tab' && 
+       e.key !== 'ArrowUp' && e.key !== 'ArrowRight' && e.key !== 'ArrowDown' && e.key !== 'ArrowLeft' &&
+       e.currentTarget.value.length === e.currentTarget.maxLength) {
+      if (index < letterRefs.length - 1) {
+        letterRefs[index + 1].current?.focus();
+      }
+    }
+  };
 
   return (
     <div className="flex space-x-6 my-4 justify-center">
-      {Array.from({ length: 5 }).map((_, index) => (
+      {letterRefs.map((ref, index) => (
         <input
           key={index}
+          ref={ref}
           type="text"
           maxLength={1}
           className="w-10 h-10 text-center border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          onKeyDown={(e) => tabClick(e.target)}
+          onKeyUp={(e) => handleKeyUp(e, index)}
         />
       ))}
     </div>
-  )
+  );
 };
 
 export default Letters;
